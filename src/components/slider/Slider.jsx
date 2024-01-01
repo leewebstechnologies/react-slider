e6562978c1d7f33d4a16e3e9ce6146a8f9ec6d31
leewebstechnologies <aliuajibade317@gmail.com>
2024-01-01 09:06:14 +0100

    completed

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -6,32 +6,60 @@ import "./slider.css";
 const Slider = () => {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0); //Go back to zero
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
   return (
     <section className="section">
       <div className="title">
         <h2>
-          <span>/</span>reviews
+          <span>/</span>react slider
         </h2>
       </div>
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
           // more stuff coming up
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+
+          if (
+            person === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
 
           return (
-              <article key={id}>
-                <img src={image} alt={name} className="person-img" />
-                <h4>{name}</h4>
-                <p className="title">{title}</p>
-                <p className="text">{quote}</p>
-                <FormatQuote className="icon" />
-              </article>
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FormatQuote className="icon" />
+            </article>
           );
         })}
-        <button className="prev">
+        <button className="prev" onClick={() => setIndex(index - 1)}>
           <ChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={() => setIndex(index + 1)}>
           <ChevronRight />
         </button>
       </div>
--- src/data.js
+++ src/data.js
@@ -1,33 +1,33 @@
 const people = [
   {
     id: 1,
-    image: "https://www.course-api.com/images/people/person-1.jpeg",
-    name: "Mariam Ibrahim",
-    title: "Office Manager",
+    image: "./assets/images/1.jpg",
+    name: "Mark Zuckerberg",
+    title: "CEO, META",
     quote:
       "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
   },
   {
     id: 2,
-    image: "https://www.course-api.com/images/people/person-4.jpeg",
-    name: "Aliu Ajibade",
-    title: "Web Developer",
+    image: "./assets/images/2.jpg",
+    name: "Bill Gates",
+    title: "CEO, Microsoft Windows",
     quote:
       "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
   },
   {
     id: 3,
-    image: "https://www.course-api.com/images/people/person-3.jpeg",
-    name: "Tunde Ajibade",
-    title: "Graphic Designer",
+    image: "./assets/images/3.jpg",
+    name: "Elon Musk",
+    title: "CEO, X",
     quote:
       "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
   },
   {
     id: 4,
-    image: "https://www.course-api.com/images/people/person-2.jpeg",
-    name: "Biola Oladapo",
-    title: "Product Designer",
+    image: "./assets/images/4.jpg",
+    name: "Steve Jobs",
+    title: "CEO, Apple",
     quote:
       "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag",
   },
